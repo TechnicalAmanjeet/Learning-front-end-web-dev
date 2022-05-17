c8457aed567f96f4cc3b455a4ed0583c39806b47
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-18 05:00:10 +0530

    done with front end part of all other web page

--- javascript/01.Color_Flipper/javascript/index.js
+++ javascript/01.Color_Flipper/javascript/index.js
@@ -1,10 +1,18 @@
 import { colorList } from './color_list.js';
 
-console.log("just for testing.");
+// console.log("just for testing.");
 
 // just for testing pourpose
+// console.log(colorList.length)
 
-console.log(colorList[2]);
+// alert("Change the background color of page by clicking on the 'change background color' button.")
+
+function pick_color_name(){
+    let random_number = parseInt( Math.random() * colorList.length );
+    return colorList[random_number];
+}
+
+// console.log(colorList[2]);
 
 
 const button = document.getElementById("change_background_color");
@@ -12,6 +20,7 @@ const background_to_change_color = document.querySelector(".main")
 const output = document.getElementById("output-value");
 
 button.addEventListener("click", () =>{
-    console.log("red");
-    background_to_change_color.style.background = "red";
+    const color = pick_color_name();
+    background_to_change_color.style.background = color;
+    output.innerText = color;
 })

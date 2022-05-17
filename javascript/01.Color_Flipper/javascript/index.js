e23d01bbde872adeb6d398a84f5b1e2c16a6620a
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-18 04:34:00 +0530

    solved the issue of import and export module in js. first we have to change the type = module in index.html or other document then the js file we want ot import in other file. we need to write this statement at the end export { variable , function_name }; and in the js file we want to import it we need to write import { variable , function_name } from './otherfile.js';

--- javascript/01.Color_Flipper/javascript/index.js
+++ javascript/01.Color_Flipper/javascript/index.js
@@ -1,1 +1,17 @@
-console.log("just for testing.")
+import { colorList } from './color_list.js';
+
+console.log("just for testing.");
+
+// just for testing pourpose
+
+console.log(colorList[2]);
+
+
+const button = document.getElementById("change_background_color");
+const background_to_change_color = document.querySelector(".main")
+const output = document.getElementById("output-value");
+
+button.addEventListener("click", () =>{
+    console.log("red");
+    background_to_change_color.style.background = "red";
+})
